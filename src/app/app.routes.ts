c47c89545453e7ea84c14dfa55a0ac16cd3da250
rocky_balboa <168601600+jsR1der@users.noreply.github.com>
2024-05-23 22:52:03 +0300

    feat: create table.component.ts,create home.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
+import {TableComponent} from "./modules/table/table.component";
+import {HomeComponent} from "./modules/home/home.component";
 
-export const routes: Routes = [{
-  path: 'table', component: AppComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  }, {
+  path: 'table', component: TableComponent
+},
+  {
+    path: '**', component: HomeComponent
+  }];
